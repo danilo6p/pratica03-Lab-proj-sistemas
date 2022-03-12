5c931b5695d1ed0b1a3d901378b6d1c3e7ee53c1
Danilo Abreu <daabreu@sga.pucminas.br>
2022-03-12 20:33:14 -0300

    Sistema de cadastro funcionando com menu
    obs.: ainda tem bug na listagem

--- sistemanovo/src/sistemanovo/Sistemanovo.java
+++ sistemanovo/src/sistemanovo/Sistemanovo.java
@@ -19,50 +19,77 @@ public class Sistemanovo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner scanner = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         
         ArrayList listaDeClientes = new ArrayList();
         Cliente usuario = new Cliente();
         int op = 0;
+        int opcao1 = 0;
+        
 
-        System.out.println("Digite 1 para cadastrar usuários ou 0 para sair !");
-         op = scanner.nextInt();
+        
 	//cpf, nome, email, senha, endereco residencial, endereco de entrega.
         
-           while(op != 0){
-		System.out.println("Bem vindo ao sistema de cadastros de usuários");
+       do {
+           System.out.println("\n \n (Digite 1 = cadastrar , Digite 2= Listar, Digite 0 = sair) ");
+            System.out.println([======>   Digite a opção desejada:  <===========]");
+             opcao1 = entrada.nextInt();
+
+            switch (opcao1) {
+            case 1:
+                System.out.println("Digite o CPF:");
+                usuario.setCpf(entrada.next());
                 
-		usuario.setCpf(scanner.nextLine());
+                System.out.println("Digite o nome do Cliente!");
+                usuario.setNome(entrada.next());
+
+                System.out.println("Digite o EMAIL:");;
+                usuario.setEmail(entrada.next());
                 
-                System.out.println("Digite CPF do usuário:");
-		usuario.setCpf(scanner.nextLine());
+                System.out.println("Digite o senha:");;
+                usuario.setSenha(entrada.next());
                 
-                System.out.println("Digite o nome do usuário:");
-		usuario.setNome(scanner.nextLine());
-		
-		 System.out.println("Digite E-MAIL:");
-		usuario.setEmail(scanner.nextLine());
+                System.out.println("Endereço de Residencia");
+                usuario.setEnderecoresidencia(entrada.next());
+
+                System.out.println("***Endereço de Entrega");
+                usuario.setEnderecoentrega(entrada.next());
+
+                listaDeClientes.add(usuario);
                 
-                System.out.println("Digite a senha");
-		usuario.setSenha(scanner.nextLine());
-		
-                
-                System.out.println("Digite o endereço residencial:");
-		usuario.setEnderecoresidencia(scanner.nextLine());
-		
-		System.out.println("Digite o endereço de entrega:");
-		usuario.setEnderecoentrega(scanner.nextLine());
+                System.out.println("\n >>>> (cadastrado) <<<<< \n ");
+                break;
+
+            case 2:
+
+                for (int i = 0; i < listaDeClientes.size(); i++) {
+                    
+                    System.out.println("CPF:"+usuario.getCpf());
                 
                
-		
-		
-		listaDeClientes.add(usuario);
-		usuario = new Cliente();
-		
-		System.out.println("Digite 1 para cadastrar usuários ou 0 para sair !");
-		op = scanner.nextInt();
-	}
+                System.out.println("NOME:"+usuario.getNome());
+                
 
+                System.out.println("EMAIL:"+usuario.getEmail());;
+               
+                
+                System.out.println("senha:"+usuario.getSenha());;
+                
+                
+                System.out.println("Endereço de Residencia:"+usuario.getEnderecoresidencia());
+                
+
+                System.out.println("Endereço de Entrega:"+usuario.getEnderecoentrega());
+			
+                
+		}
+                
+                default:
+                    if(opcao1 >2)
+                System.out.println("Opção Invalida!\n \n ");
+                break;
+            }
+        } while (opcao1 != 0);
 
     }
     
