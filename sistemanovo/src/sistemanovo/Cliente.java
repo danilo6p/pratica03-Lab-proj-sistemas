17f66a6d7ed9843d525ba41b3db0fd268d8e2531
Danilo Abreu <daabreu@sga.pucminas.br>
2022-03-12 19:31:53 -0300

    criei classe cliente com get e set
    criei classe cliente com get e set

---
+++ sistemanovo/src/sistemanovo/Cliente.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemanovo;
+
+/**
+ *
+ * @author Danilo
+ * 
+ * 
+ */
+public class Cliente {
+    
+    String cpf;
+    String nome;
+    String email;
+    String senha;
+    String enderecoresidencia;
+    String enderecoentrega;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEnderecoresidencia() {
+        return enderecoresidencia;
+    }
+
+    public void setEnderecoresidencia(String enderecoresidencia) {
+        this.enderecoresidencia = enderecoresidencia;
+    }
+
+    public String getEnderecoentrega() {
+        return enderecoentrega;
+    }
+
+    public void setEnderecoentrega(String enderecoentrega) {
+        this.enderecoentrega = enderecoentrega;
+    }
+    
+    
+    
+    
+}
