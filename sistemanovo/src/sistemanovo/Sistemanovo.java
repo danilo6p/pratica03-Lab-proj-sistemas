ef36162de5cacb24a2db6264579cd139bb56a0c3
Danilo Abreu <daabreu@sga.pucminas.br>
2022-03-12 19:53:32 -0300

    cadastro de clientes pronto (com bug)
    cadastro de cliente pronto, mas tem um buguizinho

--- sistemanovo/src/sistemanovo/Sistemanovo.java
+++ sistemanovo/src/sistemanovo/Sistemanovo.java
@@ -5,6 +5,9 @@
  */
 package sistemanovo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Danilo
@@ -16,8 +19,51 @@ public class Sistemanovo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
         
-        System.out.println("bora!!");
+        ArrayList listaDeClientes = new ArrayList();
+        Cliente usuario = new Cliente();
+        int op = 0;
+
+        System.out.println("Digite 1 para cadastrar usuários ou 0 para sair !");
+         op = scanner.nextInt();
+	//cpf, nome, email, senha, endereco residencial, endereco de entrega.
+        
+	//while(op != 0){
+		System.out.println("Bem vindo ao sistema de cadastros de usuários");
+		
+               
+                
+                System.out.println("Digite o nome do usuário:");
+		usuario.setNome(scanner.nextLine());
+                
+                 System.out.println("Digite CPF do usuário:");
+		usuario.setCpf(scanner.nextLine());
+		
+		 System.out.println("Digite E-MAIL:");
+		usuario.setEmail(scanner.nextLine());
+                
+                System.out.println("Digite a senha");
+		usuario.setSenha(scanner.nextLine());
+		
+                
+                System.out.println("Digite o endereço residencial:");
+		usuario.setEnderecoresidencia(scanner.nextLine());
+		
+		System.out.println("Digite o endereço de entrega:");
+		usuario.setEnderecoentrega(scanner.nextLine());
+                
+               
+		
+		
+		listaDeClientes.add(usuario);
+		usuario = new Cliente();
+		
+		System.out.println("Digite 1 para cadastrar usuários ou 0 para sair !");
+		op = scanner.nextInt();
+	//}
+
+
     }
     
 }
